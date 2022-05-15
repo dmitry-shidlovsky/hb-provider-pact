52e4a8918752d17b27effacc32a73de0f3af7b32
Dmitriy Shidlovsiy <dshidlovskiy@wallarm.com>
2022-05-16 00:51:35 +0200

    add MiddleName

--- model/user.go
+++ model/user.go
@@ -3,11 +3,12 @@ package model
 import "errors"
 
 type User struct {
-	FirstName string `json:"firstName" pact:"example=Dmitry"`
-	LastName  string `json:"lastName" pact:"example=Shidlovsky"`
-	Username  string `json:"username" pact:"example=dsh"`
-	Type      string `json:"type" pact:"example=admin,regex=^(admin|user|guest)$"`
-	ID        int    `json:"id" pact:"example=1"`
+	FirstName  string `json:"firstName" pact:"example=Dmitry"`
+	LastName   string `json:"lastName" pact:"example=Shidlovsky"`
+	MiddleName string `json:"middleName" pact:"example=Sergeevich"`
+	Username   string `json:"username" pact:"example=dsh"`
+	Type       string `json:"type" pact:"example=admin,regex=^(admin|user|guest)$"`
+	ID         int    `json:"id" pact:"example=1"`
 }
 
 var (
