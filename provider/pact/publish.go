94062735914357b22c1a99847b8039c60b38d2f4
Dmitriy Shidlovsiy <dshidlovskiy@wallarm.com>
2022-05-15 20:35:16 +0200

    fix publisher

--- provider/pact/publish.go
+++ provider/pact/publish.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"path/filepath"
 
 	"github.com/pact-foundation/pact-go/dsl"
 	"github.com/pact-foundation/pact-go/types"
@@ -13,7 +11,6 @@ import (
 func main() {
 	p := dsl.Publisher{}
 	err := p.Publish(types.PublishRequest{
-		PactURLs:        []string{filepath.FromSlash(fmt.Sprintf("%s/testconsumer-testprovider.json", os.Getenv("PACT_DIR")))},
 		PactBroker:      os.Getenv("PACT_BROKER_URL"),
 		ConsumerVersion: os.Getenv("PACT_SERVICE_VERSION"),
 		Tags:            []string{os.Getenv("PACT_SERVICE_TAG")},
