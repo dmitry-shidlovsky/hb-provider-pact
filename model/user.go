8aba70e39de835d1b38c4537698b24b00fe0243b
Dmitriy Shidlovsiy <dshidlovskiy@wallarm.com>
2022-05-16 00:35:01 +0200

    remove middlename

--- model/user.go
+++ model/user.go
@@ -3,12 +3,11 @@ package model
 import "errors"
 
 type User struct {
-	FirstName  string `json:"firstName" pact:"example=Dmitry"`
-	LastName   string `json:"lastName" pact:"example=Shidlovsky"`
-	MiddleName string `json:"middleName" pact:"example=Sergeevich"`
-	Username   string `json:"username" pact:"example=dsh"`
-	Type       string `json:"type" pact:"example=admin,regex=^(admin|user|guest)$"`
-	ID         int    `json:"id" pact:"example=1"`
+	FirstName string `json:"firstName" pact:"example=Dmitry"`
+	LastName  string `json:"lastName" pact:"example=Shidlovsky"`
+	Username  string `json:"username" pact:"example=dsh"`
+	Type      string `json:"type" pact:"example=admin,regex=^(admin|user|guest)$"`
+	ID        int    `json:"id" pact:"example=1"`
 }
 
 var (
