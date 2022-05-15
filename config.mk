fa0ace7183301c9411e93f82d2b677c693b224e1
Dmitriy Shidlovsiy <dshidlovskiy@wallarm.com>
2022-05-16 00:27:05 +0200

    fix make config.mk

--- config.mk
+++ config.mk
@@ -5,6 +5,5 @@ export CONSUMER_NAME = TestConsumer
 export PACT_DIR = $(PWD)/pacts
 export LOG_DIR = $(PWD)/log
 export PACT_BROKER_URL = https://hb-test.pactflow.io
-export PACT_SERVICE_VERSION = 1.0.0
 export PACT_BROKER_TOKEN = eKDpjMNjWFbJXMm0HhDHGw
 export PUBLISH_VERIFICATION_RESULTS = true
