27eaf133881c33198f401fc9e897e9805257ea94
Dmitriy Shidlovsiy <dshidlovskiy@wallarm.com>
2022-05-16 17:37:38 +0200

    remove consumer name from config

--- config.mk
+++ config.mk
@@ -1,7 +1,6 @@
 export PATH := $(PWD)/pact/bin:$(PATH)
 export PATH
 export PROVIDER_NAME = TestProvider
-export CONSUMER_NAME = TestConsumer
 export PACT_DIR = $(PWD)/pacts
 export LOG_DIR = $(PWD)/log
 export PACT_BROKER_URL = https://hb-test.pactflow.io
