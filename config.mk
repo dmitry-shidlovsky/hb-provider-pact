46cfc07e7d1661d4e437870aa8ad2dea9e0e75a4
Dmitriy Shidlovsiy <dshidlovskiy@wallarm.com>
2022-05-30 18:23:31 +0200

    remove PUBLISH_VERIFICATION_RESULTS from make file

--- config.mk
+++ config.mk
@@ -5,4 +5,3 @@ export PACT_DIR = $(PWD)/pacts
 export LOG_DIR = $(PWD)/log
 export PACT_BROKER_URL = https://hb-test.pactflow.io
 export PACT_BROKER_TOKEN = eKDpjMNjWFbJXMm0HhDHGw
-export PUBLISH_VERIFICATION_RESULTS = true
